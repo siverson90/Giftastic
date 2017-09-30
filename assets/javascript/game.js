ff41ef7edcf7a0f3bf7db998fc9d1260651b9127
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-09-30 14:05:05 -0700

    refactored logic for rendering gifs to screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,78 @@
-var countriesArr= ["United States", "France", "Brazil"];
+function renderGifs(){
+  $(".btn-primary").on("click", function(){
+      $("#imageWrapper").empty();
 
-function userInputToArr (){
-  var result = $("#countryInput").val();
-  // console.log(result);
-  
-  countriesArr.push(result);
-  // console.log(countriesArr);
-
-}
-
-function arrToBtn(){
-
-  $("#buttonWrapper").empty();
-
-  var buttonsDiv = $("<div>");
-
-  for(var i = 0; i < countriesArr.length-1; i++){
-    var newBtn = $("<button>");
-    newBtn.text(countriesArr[i]);
-    newBtn.val(countriesArr[i]);
-    newBtn.addClass("btn btn-primary");
-    buttonsDiv.append(newBtn);
-  }
-
-  $("#buttonWrapper").append(buttonsDiv);
-}
-
-  $("#submit-btn").on("click", function(event){
+    var url = "https://api.giphy.com/v1/gifs/search?q="
+    var tag = $(this).val()
+    var key= "&api_key=gPV22XzAeHPfz5eB9bl1Yom9UWYbJXix&limit=10"
     
-    event.preventDefault();
-    userInputToArr();
-    arrToBtn();
+    var queryURL= url + tag + key;
+    // console.log(queryURL);
 
-  })
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    })
+      .done(function(response){
+        // console.log(response);
 
+        result= response.data;
+        // console.log(result);
+
+        var gifsAppendDiv = $("<div>");
+
+        for( var i = 0; i < result.length; i++){
+          var gifImage = $("<img>");
+          gifImage.attr("src", result[i].images.fixed_height_small_still.url);
+          console.log(gifImage);
+          gifsAppendDiv.append(gifImage);
+        }
+        $("#imageWrapper").append(gifsAppendDiv);
+      })
+    
+    })
+}
 
   $( document ).ready(function() {
-        userInputToArr();
-        arrToBtn();
+    
+    var countriesArr= ["United States","France","Brazil"];
+    console.log(countriesArr);
+    
+    renderBtn();
 
-        $(".btn-primary").on("click", function(){
-          $("#imageWrapper").empty();
+    function renderBtn(){
 
-        var url = "https://api.giphy.com/v1/gifs/search?q="
-        var tag = $(this).val()
-        var key= "&api_key=gPV22XzAeHPfz5eB9bl1Yom9UWYbJXix&limit=10"
+      $("#buttonWrapper").empty();
+
+      var buttonsDiv = $("<div>");
+      console.log(countriesArr);
+
+      for(var i = 0; i < countriesArr.length; i++){
+        var newBtn = $("<button>");
+        newBtn.text(countriesArr[i]);
+        newBtn.val(countriesArr[i]);
+        newBtn.addClass("btn btn-primary");
+        buttonsDiv.append(newBtn);
+      }
+      $("#buttonWrapper").append(buttonsDiv);
+      renderGifs();
+    } 
+
+    $("#submit-btn").on("click", function(event){
+      event.preventDefault();
+      // userInputToArr();
+      var result = $("#countryInput").val();
+      // console.log(result);
+      
+      countriesArr.push(result);
+      // console.log(countriesArr);
+      renderBtn();
+    })
+
+    renderGifs();
+
         
-        var queryURL= url + tag + key;
-        // console.log(queryURL);
-
-        $.ajax({
-          url: queryURL,
-          method: "GET",
-        })
-          .done(function(response){
-            // console.log(response);
-
-            result= response.data;
-            // console.log(result);
-
-            var gifsAppendDiv = $("<div>");
-
-            for( var i = 0; i < result.length; i++){
-              var gifImage = $("<img>");
-              gifImage.attr("src", result[i].images.fixed_height_small_still.url);
-              console.log(gifImage);
-              gifsAppendDiv.append(gifImage);
-            }
-            $("#imageWrapper").append(gifsAppendDiv);
-          })
-        
-        })
-        
-    });
+  });
 
 
 
